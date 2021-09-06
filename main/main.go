9098e984b68e66c009aa5e47a74d1385bd102272
Samuel Neumann <samuelfneumann@gmail.com>
2021-09-06 14:21:47 -0600

    Rename Maze.go to Grid.go
    Renamed the maze file to reflect the fact that it really is not a maze,
    but rather a grid. A maze will be a grid, initialized with a certain
    wall pattern using an Initer, and then with a player.

--- main/main.go
+++ main/main.go
@@ -8,10 +8,10 @@ import (
 )
 
 func main() {
-	g := gomaze.NewGrid(10, 15)
+	g := gomaze.NewGrid(10, 20)
 
 	w := gomaze.NewAldousBroder(g, time.Now().UnixNano())
 	w.Init()
-	// fmt.Println(w)
+
 	fmt.Println(g)
 }
